128138d5ee07020492efc4c420fdec0a379569fd
yeonhashin <carbeau@naver.com>
2026-03-17 22:17:59 +0900

    init: project + docker mysql setup

---
+++ src/test/java/com/hayeon/groupbuy/GroupBuyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayeon.groupbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupBuyingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
